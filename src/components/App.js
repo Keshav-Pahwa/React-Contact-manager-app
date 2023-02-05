ea99a16ef71798799bc750d54a115b5de0478716
Keshav Pahwa <keshavpahwa01@gmail.com>
2023-02-05 10:19:18 +0530

    Updated Addcontacts and ContactList modules
    Signed-off-by: Keshav Pahwa <keshavpahwa01@gmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,26 @@
 import "./App.css";
 import Header from "./Header";
-import AddContact from "./AddContact";
+import AddContacts from "./AddContacts";
 import ContactList from "./ContactList";
 
 function App() {
+  const contacts = [
+    {
+      id: "1",
+      name: "Keshav",
+      email: "blahblah@gmail.com"
+    },
+    {
+      id: "2",
+      name: "Pahwa",
+      email: "blahblahblah@gmail.com"
+    }
+  ]
     return (
         <div className="ui container">
           <Header />
-          <AddContact />
-          <ContactList />
+          <AddContacts />
+          <ContactList contacts = {contacts}/>
         </div>
       );
     }
